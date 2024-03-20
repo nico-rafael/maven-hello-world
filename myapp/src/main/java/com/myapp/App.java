7d2ff5abf5614e43c4c2dca739e93398129d8e20
nicolasapelbaum <napelbaum@securitysolutions.opentex.com>
2024-03-20 10:07:39 +0200

    dockerFile

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!, Hello Nico" );
+        String r = new java.io.File(App.class.getProtectionDomain()
+                .getCodeSource()
+                .getLocation()
+                .getPath())
+                .getName();
+        System.out.println("Jar version: " + r);
     }
+
+
 }
